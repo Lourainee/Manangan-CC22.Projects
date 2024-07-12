9feba1f74f86bced13ecf1e5a7b3e4eefbfda00f
Lourainee <angellourainemanangan@gmail.com>
2024-07-12 19:57:44 +0800

    This file opens 5 webs, opens calcu and notepad

---
+++ BSCRIPT01.bat
@@ -0,0 +1,13 @@
+@echo off
+start https://www.webtoons.com/
+start https://web.telegram.org/
+start https://github.com/
+start https://www.facebook.com/
+start https://leonardreyes.notion.site/Platform-Technologies-1401038dae77468e9e76e0bcd20b4bc2
+
+start calc
+start notepad
+
+pause
+
+shutdown -s
